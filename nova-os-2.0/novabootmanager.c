ae77827e60f11fb31fad3d501a0757554a83c59b
ataberk320 <atatv183@gmail.com>
2025-08-03 00:06:12 +0300

    Create nova-os-2.0/novabootmanager.c

---
+++ nova-os-2.0/novabootmanager.c
@@ -0,0 +1,6 @@
+#include </usr/include/efi/efi.h>
+#include </usr/include/efi/efilib.h>
+
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+//I will edit soon...
+}
