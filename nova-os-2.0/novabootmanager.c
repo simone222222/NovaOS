89201963f7cad111ee07465009a864c3cedf7819
ataberk320 <atatv183@gmail.com>
2025-08-03 00:20:49 +0300

    Update novabootmanager.c

--- nova-os-2.0/novabootmanager.c
+++ nova-os-2.0/novabootmanager.c
@@ -1,7 +1,11 @@
+//To be continued...
 #include </usr/include/efi/efi.h>
 #include </usr/include/efi/efilib.h>
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    SystemTable SystemTable->ConOut->OutputString(SystemTable->ConOut, L"-------------------Nova Boot Manager----------------------");
-SystemTable->ConOut->OutputString(SystemTable->ConOut, L"")
+    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_CYAN);
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"---------------------Nova Boot Manager----------------------\r\n");
+    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_MAGENTA);
+
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Booting NovaOS 2.0")
 }
